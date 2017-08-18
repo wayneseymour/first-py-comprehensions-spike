6a537dcc67ca7cd1f0f3f23288e4c78e5134c7ab
Wayne Kenneth Seymour III <WayneSeymour@iheartmedia.com>
2017-08-18 16:32:05 -0400

    init commit plus an explanation in index

---
+++ index.py
@@ -0,0 +1,13 @@
+squared_ints = [ e**2 for e in a_list if type(e) == types.IntType ]
+
+# output expression
+e**2
+
+# variable
+e
+
+# sequence
+a_list
+
+# optional predicate
+if type(e) == types.IntType
